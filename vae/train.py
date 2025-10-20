bbe66c46fa506633ed3f0bec7fd59e79527d3ebe
hooved <172129504+hooved@users.noreply.github.com>
2025-10-20 19:20:44 +0000

    implement decoder

--- vae/train.py
+++ vae/train.py
@@ -1,9 +1,11 @@
 # minimal vae training implementation
 import torch
 from torch import Tensor
-from torch.nn.functional import silu, pad, scaled_dot_product_attention
+from torch.nn.functional import silu, pad, scaled_dot_product_attention, interpolate
 import helpers
 from helpers import ModuleListTyped as ModuleList
+from typing import Literal
+
 Module = helpers.ModuleCallTyped
 Conv2d, GroupNorm = helpers.Conv2dTyped, helpers.GroupNormTyped
 
@@ -39,20 +41,34 @@ class Downsample(Module):
     x = self.conv(x)
     return x
 
-class Down(Module):
-  def __init__(self, in_ch:int, out_ch:int, downsample:bool):
+class Upsample(Module):
+  def __init__(self, ch:int):
+    super().__init__()
+    self.conv = Conv2d(ch, ch, kernel_size=3, stride=1, padding=1)
+
+  def forward(self, x:Tensor) -> Tensor:
+    x = interpolate(x, scale_factor=2.0, mode="nearest")
+    x = self.conv(x)
+    return x
+
+class Step(Module):
+  def __init__(self, in_ch:int, out_ch:int, direction:Literal["down", "up", None], num_res_blocks:int):
     super().__init__()
     self.block: ModuleList[ResnetBlock] = ModuleList()
-    for x,y in ((in_ch, out_ch), (out_ch, out_ch)):
+    for x,y in [(in_ch, out_ch)] + [(out_ch, out_ch)] * (num_res_blocks - 1):
       self.block.append(ResnetBlock(x, y))
-    if downsample:
+    if direction == "down":
       self.downsample = Downsample(out_ch)
+    elif direction == "up":
+      self.upsample = Upsample(out_ch)
 
   def forward(self, x:Tensor) -> Tensor:
     for resnetblock in self.block:
       x = resnetblock(x)
     if hasattr(self, "downsample"):
       x = self.downsample(x)
+    elif hasattr(self, "upsample"):
+      x = self.upsample(x)
     return x
 
 class AttnBlock(Module):
@@ -93,9 +109,9 @@ class Encoder(Module):
   def __init__(self, in_ch:int, ch:int):
     super().__init__()
     self.conv_in = Conv2d(in_ch, ch, kernel_size=3, stride=1, padding=1)
-    self.down: ModuleList[Down] = ModuleList()
-    for in_mult, out_mult, downsample in zip((1, 1, 2, 4), (1, 2, 4, 4), (True, True, True, False)):
-      self.down.append(Down(in_mult*ch, out_mult*ch, downsample))
+    self.down: ModuleList[Step] = ModuleList()
+    for in_mult, out_mult, direction in zip((1, 1, 2, 4), (1, 2, 4, 4), ("down", "down", "down", None)):
+      self.down.append(Step(in_mult*ch, out_mult*ch, direction, 2))
 
     self.mid = Middle(in_mult*ch)
     self.norm_out = GroupNorm(num_groups=32, num_channels=in_mult*ch, eps=1e-6, affine=True)
@@ -112,8 +128,35 @@ class Encoder(Module):
     x = self.conv_out(x)
     return x
 
+class Decoder(Module):
+  def __init__(self, in_ch:int, ch:int):
+    super().__init__()
+    self.conv_in = Conv2d(in_ch, 4*ch, kernel_size=3, stride=1, padding=1)
+    self.mid = Middle(512)
+    self.up: ModuleList[Step] = ModuleList()
+    for in_mult, out_mult, direction in zip((4, 4, 2, 1), (4, 2, 1, 1), ("up", "up", "up", None)):
+      self.up.append(Step(in_mult*ch, out_mult*ch, direction, 3))
+
+    self.norm_out = GroupNorm(num_groups=32, num_channels=ch, eps=1e-6, affine=True)
+    self.conv_out = Conv2d(ch, 3, kernel_size=3, stride=1, padding=1)
+
+  def forward(self, x:Tensor) -> Tensor:
+    x = self.conv_in(x)
+    x = self.mid(x)
+    for level in range(4):
+      x = self.up[level](x)
+
+    x = self.norm_out(x)
+    x = silu(x)
+    x = self.conv_out(x)
+    return x
+
 if __name__=="__main__":
   enc = Encoder(3, 128)
   test = torch.randn(1,3,256,256)
   out = enc(test)
+
+  dec = Decoder(4, 128)
+  test = torch.randn(1,4,32,32)
+  out = dec(test)
   pause = 1
